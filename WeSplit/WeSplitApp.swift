f1965132e2a6db97147827900de88f2cd2ad3e3d
ShakiraReidThomas <kaethedev@gmail.com>
2021-12-03 14:27:37 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Shakira Reid-Thomas on 12/3/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
